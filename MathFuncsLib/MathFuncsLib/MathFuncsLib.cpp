fb0b97c81dd890e769c0e7cb20b3d7a6d67f5fc4
B17041911王梦强 <1012068860@qq.com>
2019-12-03 08:43:42 +0800

    静态库。。

---
+++ MathFuncsLib/MathFuncsLib/MathFuncsLib.cpp
@@ -0,0 +1,33 @@
+﻿// MathFuncsLib.cpp
+// post-build command: lib MathFuncsLib.obj
+
+#include "pch.h"
+#include "framework.h"
+#include "MathFuncsLib.h"
+
+#include <stdexcept>
+
+using namespace std;
+
+namespace MathFuncs
+{
+	double MyMathFuncs::Add(double a, double b)
+	{
+		return a + b;
+	}
+
+	double MyMathFuncs::Subtract(double a, double b)
+	{
+		return a - b;
+	}
+
+	double MyMathFuncs::Multiply(double a, double b)
+	{
+		return a * b;
+	}
+
+	double MyMathFuncs::Divide(double a, double b)
+	{
+		return a / b;
+	}
+}
